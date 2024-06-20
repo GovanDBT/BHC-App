822a505791a73de6d9d47feeb8af784e8ee17989
Thembi Dibotelo <govan.dibotelo@gmail.com>
2024-06-20 21:39:39 +0200

    defining the colors

---
+++ app/config/colors.js
@@ -0,0 +1,17 @@
+export default {
+    primary: "#9f3838",
+    black: "#000",
+    white: "#fff",
+    textColor: "#333",
+    lightTextColor: "#6e6969",
+    inputBorderColor: "#d9d9d9",
+    grayColor: "#f4f4f4",
+    primaryTransparent: "rgba(159, 56, 56, 0.36)",
+    primaryTransparentx2: "rgba(159, 56, 56, 0.17)",
+    greenColor: "#319f43",
+    greenTransparent: "rgba(49, 159, 67, 0.36)",
+    greenTransparentx2: "rgba(49, 159, 67, 0.17)",
+    blueColor: "#38559f",
+    blueTransparent: "rgba(56, 85, 159, 0.36)",
+    blueTransparentx2: "rgba(56, 85, 159, 0.17)"
+}
