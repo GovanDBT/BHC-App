411080d0ff9d964b470dd404676a5d4fd64a5694
Thembi Dibotelo <govan.dibotelo@gmail.com>
2024-06-20 22:13:20 +0200

    defining the screen

--- App.js
+++ App.js
@@ -2,9 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 import AppText from './app/components/AppText';
+import Screen from './app/screens/Screen';
 
 export default function App() {
   return (
-    <AppText>Hello world!!!</AppText>
+    <Screen>
+      <AppText>Hello world!!!</AppText>
+    </Screen>
   );
 }
