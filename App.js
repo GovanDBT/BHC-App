68939dbb4c58df0af1bb3e9cf1cec3af3821b349
Thembi Dibotelo <govan.dibotelo@gmail.com>
2024-06-21 21:50:58 +0200

    create a tag

--- App.js
+++ App.js
@@ -4,11 +4,15 @@ import 'react-native-gesture-handler';
 import AuthNavigator from './app/navigation/AuthNavigator';
 import NavigationTheme from './app/navigation/NavigationTheme';
 import AppNavigator from './app/navigation/AppNavigator';
+import Tag from './app/components/Tag';
+import Screen from './app/screens/Screen';
 
 export default function App() {
   return (
     <NavigationContainer theme={NavigationTheme}>
-      <AppNavigator />
+      <Screen>
+        <Tag title="Rent" />
+      </Screen>
     </NavigationContainer>
   );
 }
---
+++ app/components/Tag.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+import AppText from './AppText';
+import colors from '../config/colors';
+
+function Tag({ title }) {
+    return (
+        <View style={styles.container}>
+            <AppText style={styles.text}>{title}</AppText>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: colors.primaryTransparent,
+        width: 70,
+        paddingVertical: 7,
+        borderRadius: 30
+    },
+    text: {
+        color: colors.primary,
+        fontWeight: 'bold',
+        textAlign: 'center'
+    }
+})
+
+export default Tag;
