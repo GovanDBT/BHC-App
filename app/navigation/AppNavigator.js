c6485820a335c8a1d72b56590053b613e0607df3
Thembi Dibotelo <govan.dibotelo@gmail.com>
2024-06-21 02:55:07 +0200

    styling the tab navigation

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 import HomeScreen from '../screens/HomeScreen';
 import PaymentScreen from '../screens/PaymentScreen';
@@ -10,12 +11,57 @@ import AccountScreen from '../screens/AccountScreen';
 const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => (
-    <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Payments" component={PaymentScreen} />
-        <Tab.Screen name="Properties" component={PropertiesScreen} />
-        <Tab.Screen name="Maintenance" component={MaintenanceScreen} />
-        <Tab.Screen name="Account" component={AccountScreen} />
+    <Tab.Navigator
+        screenOptions={{
+            tabBarStyle: {
+                height: 65, // Increase the height here
+                paddingBottom: 12, // Optional: adjust padding if needed
+            },
+            tabBarLabelStyle: {
+                fontSize: 11, // Optional: adjust label size if needed
+            }
+        }}
+    >
+        <Tab.Screen 
+            name="Home" 
+            component={HomeScreen} 
+            options={{
+                tabBarIcon: ({ color, size }) =>
+                    <AntDesign name="home" size={size} color={color} />
+            }} 
+        />
+        <Tab.Screen 
+            name="Payments" 
+            component={PaymentScreen} 
+            options={{
+                tabBarIcon: ({ color, size }) =>
+                    <AntDesign name="wallet" size={size} color={color} />
+            }} 
+        />
+        <Tab.Screen 
+            name="Properties" 
+            component={PropertiesScreen} 
+            options={{
+                tabBarIcon: ({ color, size }) =>
+                    <AntDesign name="pluscircleo" size={size} color={color} />
+            }}
+        />
+        <Tab.Screen 
+            name="Maintenance" 
+            component={MaintenanceScreen} 
+            options={{
+                tabBarIcon: ({ color, size }) =>
+                    <AntDesign name="tool" size={size} color={color} />
+            }}
+        />
+        <Tab.Screen 
+            name="Account" 
+            component={AccountScreen} 
+            options={{
+                tabBarIcon: ({ color, size }) =>
+                    <AntDesign name="user" size={size} color={color} />
+            }}
+        />
     </Tab.Navigator>
 )
 
